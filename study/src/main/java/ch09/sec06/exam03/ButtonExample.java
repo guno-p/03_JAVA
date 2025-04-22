0f9aca2af71e63aca3a084655ab0e9c73d81f92e
Guno <sushieatinggg@gmail.com>
2025-04-22 14:36:33 +0900

    250422 기본 과제

---
+++ study/src/main/java/ch09/sec06/exam03/ButtonExample.java
@@ -0,0 +1,14 @@
+package ch09.sec06.exam03;
+
+import ch09.sec06.exam01.Button;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        class OkListener implements Button.ClickListener {
+            @Override
+            public void onClick() {
+                System.out.println("OK버튼을 클릭했습니다.");
+            }
+        }
+    }
+}
