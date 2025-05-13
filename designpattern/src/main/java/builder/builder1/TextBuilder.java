5c89f738a0c4d032c0d42cfbf2ed6d99639aaa2d
Guno <sushieatinggg@gmail.com>
2025-05-13 11:51:40 +0900

    250512 Builder 디자인 패턴 1

---
+++ designpattern/src/main/java/builder/builder1/TextBuilder.java
@@ -0,0 +1,37 @@
+package builder.builder1;
+
+public class TextBuilder extends Builder {
+    private StringBuilder sb = new StringBuilder();
+
+    @Override
+    public void makeTitle(String title) {
+        sb.append("=================\n");
+        sb.append("[");
+        sb.append(title);
+        sb.append("]\n\n");
+    }
+
+    @Override
+    public void makeString(String str) {
+        sb.append("■").append(str).append("\n\n");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        for (String item : items) {
+            sb.append(".");
+            sb.append(item);
+            sb.append("\n");
+        }
+        sb.append("\n");
+    }
+
+    @Override
+    public void close() {
+        sb.append("=================\n");
+    }
+
+    public String getTextResult() {
+        return sb.toString();
+    }
+}
