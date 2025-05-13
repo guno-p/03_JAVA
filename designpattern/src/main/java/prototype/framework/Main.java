3a0f3199b56eb0403e6b5d8916b7da05bb387ab3
Guno <sushieatinggg@gmail.com>
2025-05-13 11:51:14 +0900

    250512 Prototype 임시 주석 처리

--- designpattern/src/main/java/prototype/framework/Main.java
+++ designpattern/src/main/java/prototype/framework/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Manager manager = new Manager();
 
-        UnderlinePen upen = new UnderlinePen('-');
+//        UnderlinePen upen = new UnderlinePen('-');
         MessageBox mbox = new MessageBox('*');
         MessageBox sbox = new MessageBox('/');
 
-        manager.register("strong message", upen);
+//        manager.register("strong message", upen);
 
         Product p1 = manager.create("strong message");
         p1.use("Hello, woreld!");
